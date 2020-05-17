bf2df3b4758b2d41f729ff2758879bbcab5b914e
Esther Kang <estherkang14@gmail.com>
2020-05-17 05:04:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
@@ -9,7 +9,6 @@ end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
-  binding.pry
 end
 
 
