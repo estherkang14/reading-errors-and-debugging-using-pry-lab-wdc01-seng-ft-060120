1b0656f25d9ebec99ad37321d113bf74aa58cab9
Esther Kang <estherkang14@gmail.com>
2020-05-17 05:16:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,14 +1,12 @@
 # don't forget to add: require 'pry'
 
-require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    "10 * "s" + string"
+    "10 * "s" + #{string}"
   else
-    string
+    "#{string}"
   end
-  binding.pry 
 end
 
 snake_it_up("surprise")
